6efd22ba2208d5b85571e307b22e20350b69e742
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 21:46:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,5 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, 1, 1)
+
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
@@ -8,4 +10,3 @@ INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1,
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
 
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, 1, 1)
