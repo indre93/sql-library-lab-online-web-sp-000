3f6c3c283661fba98ac02a67cf68ec215919b3d2
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 22:40:45 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,8 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species
   FROM characters
-  ORDER BY(characters.species)"
+  ORDER BY(characters.species)
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
