7ba793e6abf33bb5eff511c119668291724eca87
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 21:36:46 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,47 @@
+CREATE TABLE series (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+breed TEXT, 
+net_worth INTEGER
+);
+
+CREATE TABLE subgenres (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+breed TEXT, 
+net_worth INTEGER
+);
+
+CREATE TABLE authors (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+breed TEXT, 
+net_worth INTEGER
+);
+
+CREATE TABLE books (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+breed TEXT, 
+net_worth INTEGER
+);
+
+CREATE TABLE characters (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+breed TEXT, 
+net_worth INTEGER
+);
+
+CREATE TABLE character_books (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+breed TEXT, 
+net_worth INTEGER
+);
