02a1e6d080a3708969a8479c3d237429f6e3a416
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 22:20:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,6 @@ def select_books_titles_and_years_in_first_series_order_by_year
   FROM books
   JOIN series
   ON books.series_id = series.id
-  ORDER BY books.year
   WHERE books.series_id = 1"
 end
 
