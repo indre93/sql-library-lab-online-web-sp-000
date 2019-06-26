bf2c97589190cc0d8c96948bbc00bb42274f2a3b
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 23:25:07 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ def select_series_title_with_most_human_characters
   JOIN characters ON series.id = characters.series_id
   WHERE characters.species = 'human'
   GROUP BY series.title
-  ORDER BY COUNT(characters.species)
+  ORDER BY MAX(characters.species)
   LIMIT 1"
 end
 
