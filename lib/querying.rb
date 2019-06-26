bbe7c54b4a3886308705d0b52e40eab7a29ae83d
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 23:55:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -47,5 +47,5 @@ def select_character_names_and_number_of_books_they_are_in
   JOIN character_books
   ON characters.id = character_books.character_id
   GROUP BY characters.name
-  ORDER BY MAX(characters.name)"
+  ORDER BY MAX(character_books.character_id)"
 end
