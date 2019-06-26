025641d5f664971b17353726c30dec707fe7fef1
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 23:34:17 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,6 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species)
   FROM characters
-  GROUP BY characters.species
   ORDER BY(characters.species) DESC
   LIMIT 1"
 end
